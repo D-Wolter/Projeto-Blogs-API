90dfe18a879f18baaa040a08aa05bcf95f9e0df8
D-Wolter <wolter.daniel@gmail.com>
2023-04-07 13:33:37 -0300

    feat: criação do endpoint POST /LOGIN

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
 const express = require('express');
-
-// ...
+const usersController = require('./controllers/user.controller');
 
 const app = express();
 
-// não remova ou mova esse endpoint
 app.get('/', (_request, response) => {
   response.send();
 });
 
 app.use(express.json());
 
-// ...
+app.post('/login', usersController.login);
 
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
